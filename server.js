21a9b76802ae78a06941da84e9b7e3833b4b12d2
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-12-04 00:25:18 -0300

    configure mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// start app
 const app = express();
 
+// start database
+mongoose.connect("mongodb://localhost:27017/nodeapi", { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+
+// first route
 app.get("/", (req, res) => {
   res.send("Hello World man!");
 });
