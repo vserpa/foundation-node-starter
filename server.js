ab4f09d006b45b578e73ee0cbf9ef9543f157fc7
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-12-04 00:50:58 -0300

    refactory routes to an exclusive file

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ requireDir('./src/models');
 
 const Product = mongoose.model("Product");
 
-// first route
-app.get("/", (req, res) => {
-  Product.create({
-    title: "React Native",
-    description: "Build native apps with React",
-    url: "http://github.com/facebook/react-native"
-  })
-  return res.send("Hello World!");
-});
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
