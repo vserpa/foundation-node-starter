f694b022b6039d9ef492444c0268a72f92a14386
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-12-04 20:29:20 -0200

    api to insert register

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const requireDir = require("require-dir")
+const requireDir = require("require-dir");
 
 // start app
 const app = express();
+app.use(express.json());
 
 // start database
-mongoose.connect("mongodb://localhost:27017/nodeapi", { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 // import all mongo models
-requireDir('./src/models');
+requireDir("./src/models");
 
 app.use("/api", require("./src/routes"));
 
