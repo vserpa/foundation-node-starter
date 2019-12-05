179d52c52979a849972538a0feda498873f90d3c
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-12-05 13:57:14 -0200

    configure cors to all domains

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors1");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // start app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // start database
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
